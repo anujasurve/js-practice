aab2b0a23ab6bb7a1127d5771f8e9471b62e51ff
anujasurve <96607901+anujasurve@users.noreply.github.com>
2022-05-03 12:09:03 +0530

    Update hackerrank.js

--- hackerrank.js
+++ hackerrank.js
@@ -21,3 +21,15 @@ console.log(psoratio);
 console.log(negratio);
 console.log(zeroratio);
 }
+
+
+//Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+//Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+function miniMaxSum(arr) {
+let min = Math.min(...arr);
+let max = Math.max(...arr);
+    let sum = arr.reduce((a, b) => a + b);
+console.log((sum-max) + ' ' + (sum-min));
+
+}
