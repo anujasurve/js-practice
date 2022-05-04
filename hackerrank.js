ca358fff8927a32e67543a4c2f7868dcdaeec21c
anujasurve <96607901+anujasurve@users.noreply.github.com>
2022-05-04 10:15:07 +0530

    Update hackerrank.js

--- hackerrank.js
+++ hackerrank.js
@@ -33,3 +33,25 @@ let max = Math.max(...arr);
 console.log((sum-max) + ' ' + (sum-min));
 
 }
+
+
+//Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+function timeConversion(s) {
+const ampm = s.slice(-2);
+    const hours = Number(s.slice(0, 2));
+    let time = s.slice(0, -2);
+    if (ampm === 'AM') {
+        if (hours === 12) { // 12am edge-case
+            return  time.replace(s.slice(0, 2), '00');
+        }
+        return time;
+    } else if (ampm === 'PM') {
+        if (hours !== 12) {
+            return time.replace(s.slice(0, 2), String(hours + 12));
+        } 
+        return time; // 12pm edge-case
+    }
+console.log(time)
+}
+
